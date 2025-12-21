2d1bc8f3bf6e05a9e01cf82ce582f59a72e2fcdd
Lenny <9110043+ldonnez@users.noreply.github.com>
2025-12-21 01:07:57 +0100

    chore(main): release 0.2.1

--- memo.sh
+++ memo.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-VERSION=0.2.0 # x-release-please-version
+VERSION=0.2.1 # x-release-please-version
 REPO="ldonnez/memo"
 
 ###############################################################################
